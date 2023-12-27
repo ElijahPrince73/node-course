e2d96ce9ad2536d291590042832e8045bd578657
Elijah Prince <elijahprince73@gmail.com>
2023-12-26 23:11:04 -0600

    Simple node server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-
-
+const http = require('http');
+////////////////////////////////
+///////// Files ////////
+////////////////////////////////
 //Blocking , synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
 
@@ -11,15 +13,27 @@ const fs = require('fs');
 
 // Non-blocking asynchronous way
 
-fs.readFile('./txt/start.txt','utf-8' ,(err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`,'utf-8' ,(err, data2) => {
-    console.log(data2);
-      fs.readFile(`./txt/append.txt`,'utf-8' ,(err, data3) => {
-        console.log(data3);
+// fs.readFile('./txt/start.txt','utf-8' ,(err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`,'utf-8' ,(err, data2) => {
+//     console.log(data2);
+//       fs.readFile(`./txt/append.txt`,'utf-8' ,(err, data3) => {
+//         console.log(data3);
 
-        fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8' ,(err) => {
-          console.log('File created');
-        })
-      })
-  })
+//         fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8' ,(err) => {
+//           console.log('File created');
+//         })
+//       })
+//   })
+// })
+
+////////////////////////////////
+///////// Server ////////
+////////////////////////////////
+
+const server = http.createServer((req, res) => {
+  res.end('hello world')
+})
+
+server.listen(8080, '127.0.0.1', () => {
+  console.log('listening on port 8080');
 })
